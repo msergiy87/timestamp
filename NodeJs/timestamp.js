890abaa72c4abbe94312f6e474f7dcd49c1a98c2
Sergiy M <sergiy_007@ukr.net>
2016-02-09 17:38:11 +0200

    add nodejs

---
+++ NodeJs/timestamp.js
@@ -0,0 +1,12 @@
+var http = require("http");
+var moment = require("moment");
+
+var server = http.createServer(function(req, res) {
+
+  var time = moment().unix()
+
+  res.write("<p>" + time + "</p>");
+
+  res.end();
+
+}).listen(8080);
